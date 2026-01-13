ccc492d1ca3e9a4d079998896a9ee1ca51e9e543
Carina Ioana Sirbu <carina.sirbu@singularity9.com>
2026-01-13 11:26:14 +0200

    use test containers

--- src/test/java/com/example/lab4/Lab4ApplicationTests.java
+++ src/test/java/com/example/lab4/Lab4ApplicationTests.java
@@ -2,12 +2,31 @@ package com.example.lab4;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers // enable Testcontainers
 class Lab4ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    // define the container (Spin up Postgres for this test)
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15-alpine");
+
+    // override application.yml properties to use the container's URL/User/Pass
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
+    @Test
+    void contextLoads() {
+        // the context will load successfully because it can connect to the DB
+    }
 
 }
